fbefa68bd2b10b6ead320e009ac2dad2e7f5f144
Benjamin Measor <bmeasor90@gmail.com>
2022-10-24 13:41:11 -0400

    RESTful APIs Part 1 Exercise Commit

--- csharp/module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/AuctionApp/Services/AuctionApiService.cs
+++ csharp/module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/AuctionApp/Services/AuctionApiService.cs
@@ -15,22 +15,56 @@ namespace AuctionApp.Services
 
         public List<Auction> GetAllAuctions()
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest("auctions");
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(request);
+
+            if (!response.IsSuccessful)
+            {
+                return null;
+            }
+            return response.Data;
         }
 
         public Auction GetDetailsForAuction(int auctionId)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions/{auctionId}");
+            IRestResponse<Auction> response = client.Get<Auction>(request);
+
+
+            if (!response.IsSuccessful)
+            {
+                return null;
+            }
+
+            return response.Data;
         }
 
         public List<Auction> GetAuctionsSearchTitle(string searchTerm)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions?title_like={searchTerm}"); //add the query parameter
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(request);
+
+
+            if (!response.IsSuccessful)
+            {
+                return null;
+            }
+
+            return response.Data;
         }
 
         public List<Auction> GetAuctionsSearchPrice(double searchPrice)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions?currentBid_lte={searchPrice}"); //add the query parameter
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(request);
+
+
+            if (!response.IsSuccessful)
+            {
+                return null;
+            }
+
+            return response.Data;
         }
     }
 }
--- csharp/module-2/11b_Consuming_RESTful_APIs_Part_1/lecture/HotelApp/Services/HotelApiService.cs
+++ csharp/module-2/11b_Consuming_RESTful_APIs_Part_1/lecture/HotelApp/Services/HotelApiService.cs
@@ -10,38 +10,93 @@ namespace HotelApp.Services
     {
         protected static RestClient client = null;
 
-        public HotelApiService(string apiUrl)
+        public HotelApiService(string apiUrl) //apiURL is where the API we want to interact with "lives" (http://localhost:3000) <-base url
         {
             if (client == null)
             {
-                client = new RestClient(apiUrl);
+                client = new RestClient(apiUrl); //build a client to interact with the API
             }
         }
 
-        public List<Hotel> GetHotels()
+        public List<Hotel> GetHotels() //this should be at localhost:3000/hotels
         {
-            throw new NotImplementedException();
+
+            //build a request
+            RestRequest request = new RestRequest("hotels"); //make a request to /hotels
+            //Send the request to the API
+            IRestResponse<List<Hotel>> response = client.Get<List<Hotel>>(request);
+            //IRestResponse<T> is a container for the data coming back from the API
+            //use the client (RestClient), make a GET request for a specific type of data, and use the request object that we built
+
+            if (!response.IsSuccessful) //check to see if my response was not a success so I can handle the situation
+            {
+                throw new HttpRequestException("Something went wrong communicating with the servicer! HOLY SHIT!");
+            }
+
+            return response.Data; //the data is wraped up in the response object
         }
 
-        public List<Review> GetReviews()
+        public List<Review> GetReviews() //http://localhost:3000/reviews
         {
-            throw new NotImplementedException();
+            //build a request
+            RestRequest request = new RestRequest("reviews"); //make a request to /hotels
+            //Send the request to the API
+            IRestResponse<List<Review>> response = client.Get<List<Review>>(request);
+            //IRestResponse<T> is a container for the data coming back from the API
+            //use the client (RestClient), make a GET request for a specific type of data, and use the request object that we built
+
+            if (!response.IsSuccessful) //check to see if my response was not a success so I can handle the situation
+            {
+                throw new HttpRequestException("Something went wrong communicating with the servicer! HOLY SHIT!");
+            }
+
+            return response.Data; //the data is wraped up in the response object
         }
 
-        public Hotel GetHotel(int hotelId)
+        public Hotel GetHotel(int hotelId) //http://localhost:3000/reviews/1
         {
-            throw new NotImplementedException();
+
+            RestRequest request = new RestRequest("hotels/{hotelId}"); //add the id on the end
+            IRestResponse<Hotel> response = client.Get<Hotel>(request);
+
+
+            if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException("Something went wrong communicating with the servicer! BOO HISS!");
+            }
+
+            return response.Data;
         }
 
-        public List<Review> GetHotelReviews(int hotelId)
+        public List<Review> GetHotelReviews(int hotelId) //http://localhost:3000/reviews?hotelId=1 where 1 is the hotel Id
         {
-            throw new NotImplementedException();
+            RestRequest request = new RestRequest("reviews?hotelId={hotelId}"); //add the query parameter (?) + id on the end
+            IRestResponse<List<Review>> response = client.Get<List<Review>>(request);
+
+
+            if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException("Something went wrong communicating with the servicer! BOO HISS!");
+            }
+
+            return response.Data;
         }
 
-        public List<Hotel> GetHotelsWithRating(int starRating)
+        public List<Hotel> GetHotelsWithRating(int starRating) //http://localhost:3000/hotels?stars=4
         {
-            throw new NotImplementedException();
+            RestRequest request = new RestRequest("hotels?stars={starRating}"); //add the query parameter
+            IRestResponse<List<Hotel>> response = client.Get<List<Hotel>>(request);
+
+
+            if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException("Something went wrong communicating with the servicer! BOO HISS!");
+            }
+
+            return response.Data;
         }
+    
+
 
         public City GetPublicAPIQuery()
         {
@@ -49,3 +104,4 @@ namespace HotelApp.Services
         }
     }
 }
+
